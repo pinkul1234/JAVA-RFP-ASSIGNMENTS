22204cf4940cb0399fdfacfce097a7eb8abe06ca
pinkul1234 <pinkul5555@gmail.com>
2022-04-02 23:27:49 +0530

    1.2

---
+++ Default.java
@@ -0,0 +1,17 @@
+class Default{
+	static int a;
+	static float b;
+	static double c;
+	static long d;
+	static char e;
+	static boolean f;
+	public static void main(String arg[])
+	{
+		System.out.println("int = "+a);
+		System.out.println("float = "+b);
+		System.out.println("double = "+c);
+		System.out.println("long = "+d);
+		System.out.println("char = "+e);
+		System.out.println("boolean = "+f);
+	}
+}
