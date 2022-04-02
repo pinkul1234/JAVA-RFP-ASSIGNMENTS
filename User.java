41e145d0e970e04a6472c238eed0ab146a7ec958
pinkul1234 <pinkul5555@gmail.com>
2022-04-02 23:36:02 +0530

    1.5

---
+++ User.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class User{
+	public static void main(String arg[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ENTER NAME");
+		String nm=sc.nextLine();
+		System.out.println("NAME IS: "+nm);
+	}
+}
