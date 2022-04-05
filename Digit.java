b4b8b80fc287a87490e894f45422d39b89247dfe
pinkul1234 <pinkul5555@gmail.com>
2022-04-05 14:03:34 +0530

    If else programs- Q1

---
+++ Digit.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Digit{
+	public static void main(String arg[])
+	{
+	System.out.println("Enter a number: ");
+	Scanner sc=new Scanner(System.in);
+	int number=sc.nextInt();
+	if(number==0) {
+	System.out.println("Zero");
+	}
+	else if(number==1) {
+	System.out.println("One");
+	}
+	else if(number==2) {
+	System.out.println("Two");
+	}
+	else if(number==3) {
+	System.out.println("Three");
+	}
+	else if(number==4) {
+	System.out.println("Four");
+	}
+	else if(number==5) {
+	System.out.println("Five");
+	}
+	else if(number==6) {
+	System.out.println("Six");
+	}	
+	else if(number==7) {
+	System.out.println("Seven");
+	}
+	else if(number==8) {
+	System.out.println("Eight");
+	}
+	else if(number==9) {
+	System.out.println("Nine");
+	}
+	else {
+	System.out.println("Please enter only Single digit number");
+	}
+   }
+}
